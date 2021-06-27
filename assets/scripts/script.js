db7848808651c6cf279a9767069e4f2caf312fea
Syed Zaki <syed.z.kazimi@durham.ac.uk>
2021-06-27 06:11:05 +0100

    Add search filter feature

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,9 @@
 document.querySelector(".button-container").addEventListener("click", () => {
   let text = document.getElementById("filter-jobs").value;
+  getJobs().then((jobs) => {
+    let filteredJobs = filterJobs(jobs, text);
+    showJobs(filteredJobs);
+  });
 });
 
 function getJobs() {
@@ -8,6 +12,26 @@ function getJobs() {
     .then((data) => {
       return data;
     });
+}
+
+function filterJobs(jobs, searchText) {
+  if (searchText) {
+    let filteredJobs = jobs.filter((job) => {
+      if (
+        job.roleName.toLowerCase().includes(searchText) ||
+        job.type.toLowerCase().includes(searchText) ||
+        job.company.toLowerCase().includes(searchText) ||
+        job.requirements.content.toLowerCase().includes(searchText)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return filteredJobs;
+  } else {
+    return jobs;
+  }
 }
 
 function showJobs(jobs) {
