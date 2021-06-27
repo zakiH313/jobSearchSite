c9908f93f9a8e7abeb9a479803727bf54295238f
Syed Zaki <syed.z.kazimi@durham.ac.uk>
2021-06-27 05:51:57 +0100

    populate grid with jobs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,43 @@
 document.querySelector(".button-container").addEventListener("click", () => {
   let text = document.getElementById("filter-jobs").value;
 });
+
+function getJobs() {
+  return fetch("./assets/data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+}
+
+function showJobs(jobs) {
+  let jobsContainer = document.querySelector(".jobs-container");
+  let jobsHTML = "";
+  jobs.forEach((job) => {
+    jobsHTML += `
+    <div class="job-tile">
+    <div class="top">
+      <img
+        src="${job.logo}"
+      />
+      <span class="material-icons more_horiz">more_horiz</span>
+    </div>
+    <div class="rolename">
+      <span>${job.roleName}</span>
+    </div>
+    <div class="description">
+      <span>${job.requirements.content}</span>
+    </div>
+    <div class="buttons">
+      <div class="button apply-now">Apply Now</div>
+      <div class="button">Message</div>
+    </div>
+  </div>
+  `;
+  });
+  jobsContainer.innerHTML = jobsHTML;
+}
+
+getJobs().then((data) => {
+  showJobs(data);
+});
